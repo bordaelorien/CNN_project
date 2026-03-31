3691abb4afff9221a86be22a3c2c5b7edaafc42b
BORDA_Elorien <elorien.borda@gmail.com>
2026-03-31 11:57:32 +0200

    convlayer

--- network.py
+++ network.py
@@ -3,18 +3,42 @@ import numpy as np
 class CNN:
     """Réseau de neurones convolutif"""
 
-    def __init__(self):
-        pass
+    def __init__(self, img_shape):
+        self.layers = [] #liste des couches de neurones
+        self.img_shape = (*img_shape, 3) #taille de l'image en entrée (image RGB donc la troisième dimension est 3)
     
 
-    def addLayer(self, type, *args):
+    def addLayer(self, type, **kwargs):
         """Ajoute une couche de neurones au CNN.
 
         Args:
             type (str): type de couche à ajouter. 4 types: "CONV", "POOL", "RELU", "FC".
             Les autres arguments sont les hyperparamètres de la couche.
         """
-        pass
+        
+        #création d'une nouvelle couche du type spécfifié
+        if type == "CONV":
+            #taille de couche précédente si elle existe, sinon taille de l'image en entrée
+            input_shape = self.layers[-1].shape if len(self.layers) > 0 else self.img_shape
+            new_layer = ConvLayer(input_shape, **kwargs)
+
+        elif type == "POOL":
+            new_layer = PoolLayer(**kwargs)
+
+        elif type == "RELU":
+            new_layer = ReluLayer()
+
+        elif type == "FC":
+            #taille de couche précédente si elle existe, sinon taille de l'image en entrée
+            input_shape = self.layers[-1].shape if len(self.layers) > 0 else self.img_shape
+            new_layer = FCLayer(input_shape, **kwargs)
+
+        else:
+            raise ValueError(f"'{type}' n'est pas un type de couche correcte. " \
+                            "Les quatre types de couches sont 'CONV', 'POOL', 'RELU', 'FC'.")
+
+        #ajout de la couche à la liste
+        self.layers.append(new_layer)
 
     
     def forwardProp(self, input_image):
@@ -27,7 +51,19 @@ class CNN:
             1D: Output du CNN pour l'image donnée.
                 Applique un softmax à la dernière couche du réseau pour obtenir un vecteur de probabilités en sortie.
         """
-        pass
+        #vérification de la taille de l'image en entrée
+        if input_image.shape != self.img_shape:
+            raise ValueError(f"Image en entrée de taille {input_image.shape} alors " \
+                             f"qu'elle devrait être de taille {self.img_shape}.")
+
+        #forward propagation à travers toutes les couches du réseau
+        activations = input_image
+        for layer in self.layers:
+            activations = layer.forward(activations)
+        
+        return self.softmax(activations)
+        
+
 
     
     def predict(self, input_image):
@@ -40,6 +76,20 @@ class CNN:
             int: Label prédit par le CNN.
         """
         return np.argmax(self.forwardProp(input_image))
+    
+
+    def softmax(self, x):
+        """Fonction softmax.
+
+        Args:
+            x (1D array): valeurs d'activations de la dernière couche (qui est sensée être une couche FC).
+
+        Returns:
+            1D array: version modifiée du vecteur en entrée qui permet de l'interpréter comme une
+                distribution de probabilités.
+        """
+        exp_x = np.exp(x - np.max(x))
+        return exp_x / exp_x.sum(axis=0)
 
 
 
@@ -47,20 +97,44 @@ class CNN:
 
 
 
-class convLayer:
+class ConvLayer:
     """Une couche de neurones convolutive"""
 
-    def __init__(self, input_shape, num_filters, *, filter_size=3, stride=1, padding="zeros"):
+    def __init__(self, input_shape, *, num_filters, filter_size=3, stride=1, padding="zeros"):
         """
         Args:
             input_shape (tuple à trois éléments): Dimensions de la couche précédente
             num_filters (int): Nombre de filtres
             filter_size (int): Taille des filtres. Defaults to 3.
             stride (int): Stride. Defaults to 1.
-            padding ("none", "zeros" ou "extend"): Type de padding appliqué à l'input. Le padding, s'il y en a,
+            padding ("none", "zeros" ou "edge"): Type de padding appliqué à l'input. Le padding, s'il y en a,
                 est calculé de façon à ce que l'output de la couche soit de la même taille que l'intput. Defaults to "zeros".
         """
-        pass
+        self.num_filters = num_filters
+        self.filter_size = filter_size
+        self.stride = stride
+        self.padding_mode = padding
+
+        #calcul des dimensions de l'output et du padding
+        W1, H1, D1 = input_shape
+
+        if padding == "none":
+            W2 = 1 + (W1 - filter_size) // stride
+            H2 = 1 + (H1 - filter_size) // stride
+            D2 = num_filters
+            self.padding = (0, 0)
+
+        elif padding == "zeros" or padding == "edge":
+            W2, H2, D2 = W1, H1, num_filters
+            pad_w = ((W1 - 1) * stride + filter_size - W1) // 2
+            pad_h = ((H1 - 1) * stride + filter_size - H1) // 2
+            self.padding = (pad_w, pad_h)
+
+        self.output_shape = (W2, H2, D2)
+
+        #initialisation des filtres et des biais
+        self.filters = np.random.randn(num_filters, filter_size, filter_size, D1)
+        self.biases = np.random.randn(num_filters, 1)
 
 
     def forward(self, input):
@@ -72,13 +146,39 @@ class convLayer:
         Returns:
             array 3D de dim W2 x H2 x D2 : Valeurs d'activation de cette couche. 
                 Si padding="none", alors l'output a les mêmes dimensions que l'input. 
-                Sinon W2 = 1 + (W2 - filter_size)/stride, H2 = 1 + (H2 - filter_size)/stride, D2 = num_filters
+                Sinon W2 = 1 + (W1 - filter_size)/stride, H2 = 1 + (H1 - filter_size)/stride, D2 = num_filters
         """
-        pass
+        x_col = self.im2col(input, self.output_shape, self.filter_size, self.stride, self.padding, self.padding_mode)
+        f_row = self.filters.reshape(self.num_filters, -1)
+        output = f_row @ x_col + self.biais
+        self.activations = output.reshape(self.output_shape)
+
+    
+    def im2col(self, input, output_shape, filter_size, stride, padding, padding_mode):
+        if padding_mode == "zeros":
+            x = np.pad(input, ((padding[0], padding[0]), (padding[1], padding[1]), (0, 0)), mode="constant")
+
+        elif padding_mode == "edge":
+            x = np.pad(input, ((padding[0], padding[0]), (padding[1], padding[1]), (0, 0)), mode="edge")
+        
+        #dimensions de l'input après padding 
+        W, H, C = x.shape
+
+        #dimenions de l'output
+        F = filter_size
+        W2, H2, D2 = output_shape
+
+        x_col = np.zeros((F * F * C, W2 * H2))
+
+        for colonne in range(W2 * H2):
+            for i in range(0, W - F + 1, stride):
+                for j in range(0, H - F + 1, stride):
+                    x_col[:, colonne] = x[i:i+F, j:j+F, :].reshape(-1)
+        
+        return x_col
 
 
-
-class poolLayer:
+class PoolLayer:
     """Une couche de max pooling"""
 
     def __init__(self, *, pooling_size=2, stride=2):
@@ -94,7 +194,7 @@ class poolLayer:
         """Applique une opération de max pooling à l'input.
 
         Args:
-            input (array 3D de dim W1 x H1 x D1): Activations de la couche précédente
+            input (array 3D de dim W1 x H1 x D1): Activations de la couche précédente.
 
         Returns:
             array 3D de dim W2 x H2 x D2 : Valeurs d'activation de cette couche.  
@@ -103,7 +203,7 @@ class poolLayer:
         pass
 
 
-class reluLayer:
+class ReluLayer:
     """Une couche ReLu"""
 
     def __init__(self):
@@ -114,12 +214,12 @@ class reluLayer:
         """Applique la fonction ReLU à l'input.
 
         Args:
-            input (3D array): Activations de la couche précédente
+            input (3D array): Activations de la couche précédente.
 
         Returns:
             3D array: Valeurs d'activations de cette couche. Mêmes dimensions que l'input.
         """
-        return 0
+        return np.maximum(input, 0)
 
 
 class FCLayer:
@@ -139,9 +239,17 @@ class FCLayer:
         précédente.
 
         Args:
-            input (3D array): Activations de la couche précédente
+            input (3D array): Activations de la couche précédente.
 
         Returns:
             1D array: Valeurs d'activations de cette couche. Taille = num_neurons.
         """
         pass
+
+
+
+cnn = CNN(img_shape=(300,300))
+cnn.addLayer(type="CONV", num_filters=96)
+img = np.zeros((32, 32, 3))
+cnn.forwardProp(img)
+print(cnn.layers)
